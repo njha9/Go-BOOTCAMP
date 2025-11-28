4ea59ef7f470f4e279a0d3323adf5a03604f81d6
niteshjha <nitesh.jha@rapido.bike>
2025-11-28 20:20:41 +0530

    GoTraining-001

---
+++ customerRating.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+
+// type CustomerRating struct {
+// 	CustomerID string
+// 	Rating     int
+// 	Comment    string
+// }
+
+// func NewCustomerRating(customerID string, rating int, comment string) *CustomerRating {
+// 	return &CustomerRating{
+// 		CustomerID: customerID,
+// 		Rating:     rating,
+// 		Comment:    comment,
+// 	}
+// }
+// func (cr *CustomerRating) IsPositive() bool {
+// 	return cr.Rating >= 4
+// }
+
+// func (cr *CustomerRating) IsNegative() bool {
+// 	return cr.Rating <= 2
+// }
+
+// func (cr *CustomerRating) Summary() string {
+// 	return fmt.Sprintf("Customer %s rated %d: %s", cr.CustomerID, cr.Rating, cr.Comment)
+// }
