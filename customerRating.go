3dab13214b37ed3aeee7773b816f4680a803b49a
niteshjha <nitesh.jha@rapido.bike>
2025-12-29 21:30:25 +0530

    customerRating

--- customerRating.go
+++ customerRating.go
@@ -1,28 +1,35 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// type CustomerRating struct {
-// 	CustomerID string
-// 	Rating     int
-// 	Comment    string
-// }
+type CustomerRating struct {
+	CustomerID string
+	Rating     int
+	Comment    string
+}
 
-// func NewCustomerRating(customerID string, rating int, comment string) *CustomerRating {
-// 	return &CustomerRating{
-// 		CustomerID: customerID,
-// 		Rating:     rating,
-// 		Comment:    comment,
-// 	}
-// }
-// func (cr *CustomerRating) IsPositive() bool {
-// 	return cr.Rating >= 4
-// }
+func NewCustomerRating(customerID string, rating int, comment string) *CustomerRating {
+	return &CustomerRating{
+		CustomerID: customerID,
+		Rating:     rating,
+		Comment:    comment,
+	}
+}
+func (cr *CustomerRating) IsPositive() bool {
+	return cr.Rating >= 4
+}
 
-// func (cr *CustomerRating) IsNegative() bool {
-// 	return cr.Rating <= 2
-// }
+func (cr *CustomerRating) IsNegative() bool {
+	return cr.Rating <= 2
+}
 
-// func (cr *CustomerRating) Summary() string {
-// 	return fmt.Sprintf("Customer %s rated %d: %s", cr.CustomerID, cr.Rating, cr.Comment)
-// }
+func (cr *CustomerRating) Summary() string {
+	return fmt.Sprintf("Customer %s rated %d: %s", cr.CustomerID, cr.Rating, cr.Comment)
+}
+
+func main() {
+	rating := NewCustomerRating("C123", 5, "Excellent service!")
+	fmt.Println(rating.Summary())
+	fmt.Println("Is Positive:", rating.IsPositive())
+	fmt.Println("Is Negative:", rating.IsNegative())
+}
